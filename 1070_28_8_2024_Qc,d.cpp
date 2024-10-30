7d425187df0c1c993e6c5bc3c56623406d02f2dc
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2024-10-30 15:18:49 +0530

    c progs included

--- 1070_28_8_2024_Qc,d.cpp
+++ 1070_28_8_2024_Qc,d.cpp
@@ -1,57 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int sum(int arr[], int start, int end);
 
-
-void verifier(int* arr, int idealWt){
-    //time complexity of the following function is O(n)
-
-    //a. Compute the (time) complexity of your algorithm/program by assuming the known weight of a correct coin.
-
-    //c.Compute the (time) complexity of your algorithm/program by assuming the known weight of a correct coin and extending the case to the number of counterfeit coin more than one (01).
-    for (int i=1;i<16;i++){
-        if(arr[i]!= idealWt){
-            cout<< "coin " << i <<" is a counterfiet coin "<<endl;
+void verifier(int *arr, int idealWt)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        if (arr[i] != idealWt)
+        {
+            cout << "coin " << i << " is a counterfeit coin " << endl;
         }
     }
 }
 
-void ukverifiy(int l, int h){
-    while(l!=h){
-        int m = (l+h)/2;
-        if(sum(l,m)<sum(m+1, h))
-            h=m;
+int ukverifiy(int arr[], int l, int h)
+{
+    while (l < h)
+    {
+        int m = (l + h) / 2;
+        if (sum(arr, l, m) < sum(arr, m + 1, h))
+            h = m;
         else
-            l= m+1;
-    }return l;
+            l = m + 1;
     }
-        
-    
-
+    return l;
 }
 
-int sum(int arr[]){
-    int size = sizeof(arr)-1;
-    int i=0, sum =0;
-    for(i; i<size; i++){
-        sum = sum +arr[i];
+int sum(int arr[], int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += arr[i];
     }
     return sum;
 }
 
-
-
-
-int main(){
+int main()
+{
     int arr[16];
-    for (int i=0; i<16; i++){
-        cout << "Enter the wt of coin";
-        cin>>arr[i];
+    for (int i = 0; i < 16; i++)
+    {
+        cout << "Enter the weight of coin " << i + 1 << ": ";
+        cin >> arr[i];
     }
     int idealWt;
-    cout <<"Enter the value of ideal Wt of coin";
+    cout << "Enter the value of the ideal weight of the coin: ";
     cin >> idealWt;
     verifier(arr, idealWt);
-    
-    
+
+    // Example of using ukverify function
+    int counterfeitIndex = ukverifiy(arr, 0, 15);
+    cout << "The counterfeit coin is likely at index: " << counterfeitIndex << endl;
+    return 0;
 }
